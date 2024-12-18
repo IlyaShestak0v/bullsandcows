99f75f26bb17617c41d63113ef6416ded86d0f19
Ilya <serega123gora@gmail.com>
2024-12-18 14:23:31 +0500

    Добавление основной части

--- code.cpp
+++ code.cpp
@@ -6,4 +6,27 @@ int main(){
   setlocale(LC_ALL, "Russian");
   srand(time(NULL));
   int s = rand() % 9000 + 1000;
+  int g, bulls, cows;
+  cout << "Добро пожаловать в игру!";
+  do
+  {
+    bulls = cows = 0;
+    cout << "Введите четырехзначное число: ";
+    cin >> g;
+    int A[4], B[4];
+    for (int i = 0; i < 4; i++)
+    {
+        A[i] = (s / (int)pow(10,3-i)) % 10;
+        B[i] = (g / (int)pow(10,3-i)) % 10;
+        if (A[i] == B[i]) bulls++;
+    }
+    for (int i = 0; i <4; i++)
+    {
+        for (int j = 0; j < 4; j++)
+        {
+            if (i != j && B[i] == A[j]) cows++;
+        }
+    }
+  }
 }
+
